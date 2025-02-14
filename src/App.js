b8ee598ef0b72786cecea9319d07865243c5e581
kadelcode <kadelcode@gmail.com>
2025-02-14 10:32:19 +0100

    Add function to handle form submission

--- src/App.js
+++ src/App.js
@@ -9,6 +9,21 @@ export default function App() {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   }
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.send(
+      process.env.REACT_APP_EMAILJS_SERVICE_ID,
+      process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+      formData,
+      process.env.REACT_APP_EMAILJS_USER_ID
+    ).then(
+      () => alert("Message sent successfully")
+    ).catch(
+      () => alert("Failed to send message.")
+    );
+  }
   return (
     <motion.div initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{ duration: 4}}>
       <h1>Welcome to My Portfolio</h1>
