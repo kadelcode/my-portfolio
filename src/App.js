ace0e62e08d68b36e072bad6ffa640fae0c3b674
kadelcode <kadelcode@gmail.com>
2025-02-14 09:58:15 +0100

    Add useState hook to manage form data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { motion } from "framer-motion";
 import emailjs from "emailjs-com";
 import "./styles.css"
 
-function App() {
+export default function App() {
+  const [formData, setFormData] = useState({name: "", email:"", message: "" });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
   return (
     <motion.div initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{ duration: 4}}>
       <h1>Welcome to My Portfolio</h1>
     </motion.div>
   )
 }
-export default App;
